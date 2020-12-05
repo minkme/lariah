1f3716958bbd6e9545cfeb702e4b3b5e87de29a8
jungmink <jungmink@sktelecom.com>
2020-12-06 00:34:47 +0900

    배포 테스트

---
+++ scripts/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=lairah
+
+echo "> Build 파일 복"
+
+cd $REPOSITORY/zip/*.jar $REPOSITORY/
+
+
+echo " > 구동중인 pid 확인"
+
+CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
+
+echo " > 현 구동 PID: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+	echo "> 구동 중 없음"
+else
+	echo "> kill -15 $CURRENT_PID"
+	kill -15 $CURRENT_PID
+	sleep 5
+fi
+
+echo "> 새로 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+
+echo "> JAR NAME: $JAR_NAME"
+
+chmod +x $JAR_NAME
+
+
+nohup java -jar -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
